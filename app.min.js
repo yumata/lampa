9e6ba3806c3a917888a3d51310fbfd5d793b8a7d
Yamata <yumatakunsan@gmail.com>
2021-10-20 17:30:55 +0300

    Fix size

--- app.min.js
+++ app.min.js
@@ -2156,7 +2156,7 @@
       var fs = sz[sl];
       $('body').css({
         fontSize: Math.max(window.innerWidth / 84.17 * fs, 10.6) + 'px'
-      });
+      }).removeClass('size--small size--normal size--bigger').addClass('size--' + sl);
     }
 
     function update$5() {
