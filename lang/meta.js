81367853cd85f8168146d9541697290ff3e1b37d
yumata <91452496+yumata@users.noreply.github.com>
2023-10-05 13:50:29 +0300

    Added translations in files

---
+++ lang/meta.js
@@ -0,0 +1,53 @@
+export default {
+    languages: {
+        ru: {
+            code: 'ru',
+            name: 'Русский',
+
+            lang_choice_title: 'Добро пожаловать',
+            lang_choice_subtitle: 'Выберите свой язык',
+        },
+        en: {
+            code: 'en',
+            name: 'English',
+
+            lang_choice_title: 'Welcome',
+            lang_choice_subtitle: 'Choose your language',
+        },
+        uk: {
+            code: 'uk',
+            name: 'Українська',
+
+            lang_choice_title: 'Ласкаво просимо',
+            lang_choice_subtitle: 'Виберіть мову',
+        },
+        be: {
+            code: 'be',
+            name: 'Беларуская',
+
+            lang_choice_title: 'Сардэчна запрашаем',
+            lang_choice_subtitle: 'Выберыце сваю мову',
+        },
+        zh: {
+            code: 'zh',
+            name: '简体中文',
+
+            lang_choice_title: '欢迎',
+            lang_choice_subtitle: '选择你的语言',
+        },
+        pt: {
+            code: 'pt',
+            name: 'Português',
+
+            lang_choice_title: 'Bem-vindo',
+            lang_choice_subtitle: 'Escolhe o teu idioma',
+        },
+        bg: {
+            code: 'bg',
+            name: 'Български',
+
+            lang_choice_title: 'Здравейте',
+            lang_choice_subtitle: 'Изберете вашият език',
+        }
+    }
+}
